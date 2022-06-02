417c6ad5b2f0e93e1d366c28acbf6f735f19a475
Aswathy-mp <aswathymp24@gmail.com>
2022-06-02 09:21:24 +0530

    edit-for-addwork-form-with-validation

--- FrontEnd/src/app/addworkform/addworkform.component.ts
+++ FrontEnd/src/app/addworkform/addworkform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-addworkform',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddworkformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  addWorkForm=this.fb.group({
+    name:new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/)]),
+    email:new FormControl('',[Validators.required,Validators.pattern(/^[a-z0-9.%+]+@[a-z0-9.-]+\.[a-z]{2,4}/)]),
+    contact:new FormControl('',[Validators.required,Validators.pattern(/^([0-9]{3})([-. ]?)([0-9]{3})([-. ]?)([0-9]{4})$/), Validators.maxLength(12)]),
+    address:new FormControl('',[Validators.required]),
+    paintingname: new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/)]),
+    category:new FormControl('',[Validators.required]),
+    price:new FormControl('',[Validators.required,Validators.pattern(/^[0-9]+$/)]),
+    description:new FormControl('',[Validators.required]),
+    image:new FormControl('',[Validators.required])
+  })
 
   ngOnInit(): void {
   }
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -59,7 +61,9 @@ import { AddworkformComponent } from './addworkform/addworkform.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
